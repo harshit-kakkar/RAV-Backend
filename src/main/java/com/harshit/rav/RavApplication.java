eac0da64b6fb219e1f4f80110bd3746b266ee534
harshit-kakkar <kharshit0@gmail.com>
2021-08-01 23:04:56 +0530

    Initial commit

---
+++ src/main/java/com/harshit/rav/RavApplication.java
@@ -0,0 +1,13 @@
+package com.harshit.rav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harshit/rav/RavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshit.rav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
